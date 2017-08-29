734481100ef5054ec9e78166ea938def0a1acb0d
marska <martin.skarsaune@kantega.no>
2017-08-29 23:41:30 +0200

    Visual adjustments

--- hawtio-web/src/main/webapp/app/spring-beans/js/springBeans.ts
+++ hawtio-web/src/main/webapp/app/spring-beans/js/springBeans.ts
@@ -11,31 +11,45 @@ module SpringBeans {
         var beansHandled = 0;
         var connection = $routeParams.con;
 
-        function parseName(mbeanReference: string) {
-            var parsedName = /.+?:name=(.+)/.exec(mbeanReference);
-            if (parsedName && parsedName[1]) {
-                return parsedName[1];
-            } else {
-                return null;
+        function detectBeanRefs(objectStructure, callback) {
+            if (objectStructure && objectStructure.beanref) {
+                var refname = parseName(objectStructure.beanref.objectName);
+                if (refname) {
+                    callback(refname);
+                }
             }
+            else if (typeof objectStructure === 'object') {
+                for (var property in objectStructure) {
+                    if (objectStructure.hasOwnProperty(property)) {
+                        detectBeanRefs(objectStructure[property], callback);
+                    }
+                }
+            }
+        }
+
+        function parseName(mbeanReference: string) {
+            return Core.parseMBean(mbeanReference).attributes['name'];
         }
         function addLinksAndDetails(response, nameToNode, graphBuilder) {
             var name = parseName(response.request.mbean);
             var node = nameToNode[name];
 
             for (var property in response.value) {
-                var propertyValue = response.value[property]
-                if (propertyValue && propertyValue.beanref) {
-                    var referencedBeanName = parseName(propertyValue.beanref.objectName);
-                    if (referencedBeanName) {
-                        graphBuilder.addLink(name, referencedBeanName, 'uses');
+                var propertyValue = response.value[property];
+                var handledByReferences = false;
+                if (propertyValue) {
+                    detectBeanRefs(propertyValue, (beanName) => {
+                        graphBuilder.addLink(name, beanName, 'uses');
+                        handledByReferences = true;
+                    });
+
+                    if (!handledByReferences) {
+                        if (!node.popup) {
+                            node.popup = { title: 'Spring bean[' + name + ']' };
+                            node.popup.content = '';
+                        }
+                        node.popup.content += property + '=' + propertyValue + '<br>';
                     }
-                } else {
-                    if (!node.popup) {
-                        node.popup = { title: 'Spring bean[' + name + ']' };
-                        node.popup.content = '';
-                    }
-                    node.popup.content += property + '=' + propertyValue + '<br>';
                 }
             }
             beansHandled++;
@@ -46,7 +60,7 @@ module SpringBeans {
             }
 
         }
-    
+
         $scope.updateGraph = () => {
 
             var graphBuilder = new ForceGraph.GraphBuilder();
@@ -58,26 +72,29 @@ module SpringBeans {
                     var childFolder = folder.children[i];
                     if (childFolder.objectName) {
                         var name = childFolder.entries.name;
-                        var url = '#/jmx/attributes?nid=' + childFolder.key;
-                        //workaround: for d3 the usual url handling of hawtio does not seem to be enforced
-                        //therefore manually add con if required
-                        if (connection) {
-                            url += '&con=' + connection;
-                        }
-                        var node = { id: name, name: name, navUrl: url, image: { url: 'img/icons/spring/spring-logo.png' , width: 32, height:32} };
-                        graphBuilder.addNode(node);
-                        nameToNode[name] = node;
-                        if (name.indexOf(':') == -1) {
-                            requests.push({ type: 'read', mbean: childFolder.objectName, arguments: [] });
+                        if (!$scope.filter || !$scope.filter.pattern || !!name.match(new RegExp($scope.filter.pattern)) === !$scope.filter.exclude) {
+                            var url = '#/jmx/attributes?nid=' + childFolder.key;
+                            //workaround: for d3 the usual url handling of hawtio does not seem to be enforced
+                            //therefore manually add con if required
+                            if (connection) {
+                                url += '&con=' + connection;
+                            }
+                            var node = { id: name, name: name, navUrl: url, image: { url: 'img/icons/spring/spring-logo.png', width: 32, height: 32 } };
+                            graphBuilder.addNode(node);
+                            nameToNode[name] = node;
+                            if (name.indexOf(':') == -1) {
+                                requests.push({ type: 'read', mbean: childFolder.objectName, arguments: [] });
+                            }
                         }
                     }
                 }
                 beanCount = requests.length;
+                beansHandled = 0;
                 jolokia.request(requests, onSuccess((response) => addLinksAndDetails(response, nameToNode, graphBuilder)));
             }
         };
 
 
-    $scope.updateGraph();
-}]);
+        $scope.updateGraph();
+    }]);
 }
